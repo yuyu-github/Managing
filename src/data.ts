a63ce0c68ee9a2ce2f4ddf600762fce12872ed30
yuyu <hcsd7b34@gmail.com>
2022-08-10 10:53:36 +0900

    Add: setDataに代入演算を実装

--- src/data.ts
+++ src/data.ts
@@ -1,20 +1,6 @@
 import * as fs from 'fs';
 
-export function setData(guildId: string | null, path: string[], value: Object): void {
-  if (guildId == null) return;
-
-  let fileName = './data/' + guildId + '.json'
-  let data: object = fs.existsSync(fileName) ? JSON.parse(fs.readFileSync(fileName).toString()) : {}
-  let parent = data;
-  path.forEach((key, i) => {
-    if (i == path.length - 1) parent[key] = value;
-    else {
-      if (parent[key] == null) parent[key] = {};
-      parent = parent[key];
-    }
-  });
-  fs.writeFileSync(fileName, JSON.stringify(data));
-}
+type operatorString = '+' | '-' | '*' | '/' | '%' | '**' | '&&' | '||' | '??';
 
 export function getData(guildId: string | null, path: string[]): Object | null {
   if (guildId == null) return null;
@@ -37,6 +23,50 @@ export function getData(guildId: string | null, path: string[]): Object | null {
     }
   });
   return value;
+}
+
+export function setData(guildId: string | null, path: string[], value: any, calc: operatorString | ((old: Object | null, val: Object | null) => Object | null) | null = null): void {
+  if (guildId == null) return;
+
+  let fileName = './data/' + guildId + '.json'
+  let data: object = fs.existsSync(fileName) ? JSON.parse(fs.readFileSync(fileName).toString()) : {}
+  let parent = data;
+  path.forEach((key, i) => {
+    if (i == path.length - 1) {
+      let newValue: any = parent[key] ?? null;
+
+      if (typeof calc == 'function') {
+        newValue = calc(newValue, value);
+      } else if (calc != null) {
+        switch (calc) {
+          case '+': newValue += value; break;
+          case '||': newValue ||= value; break;
+          case '&&': newValue &&= value; break;
+          case '??': newValue ??= value; break;
+        }
+        if (typeof newValue == 'number') {
+          switch (calc) {
+            case '-': newValue -= value; break;
+            case '*': newValue *= value; break;
+            case '/': newValue /= value; break;
+            case '%': newValue %= value; break;
+            case '**': newValue **= value; break;
+          }
+        }
+
+        if (typeof newValue == 'number' && Number.isNaN(newValue)) newValue = 0;
+      } else {
+        newValue = value;
+      }
+
+      parent[key] = newValue;
+    }
+    else {
+      if (parent[key] == null) parent[key] = {};
+      parent = parent[key];
+    }
+  });
+  fs.writeFileSync(fileName, JSON.stringify(data));
 }
 
 export function deleteData(guildId: string | null, path: string[]): void {
