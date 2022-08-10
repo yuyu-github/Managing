fd882c86d4db664141ec7d9da75bfd8be91b2347
yuyu <hcsd7b34@gmail.com>
2022-08-10 15:39:34 +0900

    Add: member-dataにランク機能を実装

--- src/commands/processes/member_data.ts
+++ src/commands/processes/member_data.ts
@@ -6,16 +6,18 @@ export async function memberData(client: Client, interaction: CommandInteraction
   const user = interaction.options.getUser('user') ?? interaction.user;
 
   const memberData = getData(interaction.guildId, ['memberData']);
-  const getAction = name => memberData?.['action']?.[name]?.[user.id] ?? 0;
-  const getTime = name => minutesToString(memberData?.['time']?.[name]?.[user.id] ?? 0);
-  const minutesToString = minutes => (minutes >= 60 ? Math.floor(minutes / 60) + '時間' : '') + minutes % 60 + '分'
+  const getAction = (name, unit) => `${memberData?.['action']?.[name]?.[user.id] ?? 0}${unit} (#${getRank(memberData?.['action']?.[name])})`;
+  const getTime = name => `${minutesToString(memberData?.['time']?.[name]?.[user.id] ?? 0)} (#${getRank(memberData?.['time']?.[name])})`;
+  const minutesToString = minutes => (minutes >= 60 ? Math.floor(minutes / 60) + '時間' : '') + minutes % 60 + '分';
+  const getRank = list => list == null ? 1 : list[user.id] == null ? Object.keys(list).length + 1 : Object.keys(list).sort((a, b) => list[b] - list[a]).indexOf(user.id) + 1;
+
   const displayData = {
-    'メッセージを送った回数': getAction('sendMessage') + '回',
-    'コマンドを使った回数': getAction('useCommand') + '回',
-    'コンテキストメニューを使った回数': getAction('useContextMenu') + '回',
-    'リアクションをした回数': getAction('addReaction') + '回',
-    'ボイスチャンネルに入った回数': getAction('joinVoiceChannel') + '回',
-    'ステージチャンネルに入った回数': getAction('joinStageChannel') + '回',
+    'メッセージを送った回数': getAction('sendMessage', '回'),
+    'コマンドを使った回数': getAction('useCommand', '回'),
+    'コンテキストメニューを使った回数': getAction('useContextMenu', '回'),
+    'リアクションをした回数': getAction('addReaction', '回'),
+    'ボイスチャンネルに入った回数': getAction('joinVoiceChannel', '回'),
+    'ステージチャンネルに入った回数': getAction('joinStageChannel', '回'),
     'ボイスチャンネルに入っていた時間': getTime('inVoiceChannel'),
     'ステージチャンネルに入っていた時間': getTime('inStageChannel'),
   }
