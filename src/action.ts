f40ae023dc693209415015440ce7404256d12bc9
yuyu <hcsd7b34@gmail.com>
2022-08-10 18:28:58 +0900

    Fix: lastJoinVoiceChannelがメンバーごとにない

--- src/action.ts
+++ src/action.ts
@@ -11,19 +11,19 @@ type actionType =
 | 'leftVoiceChannel'
 | 'leftStageChannel'
 
-let lastJoinVoiceChannel: number | null = null;
-let lastJoinStageChannel: number | null = null;
+let lastJoinVoiceChannel = {};
+let lastJoinStageChannel = {};
 
 export function action(guildId: string | null, userId: string, type: actionType) {
   setData(guildId, ['memberData', 'action', type, userId], 1, '+');
   
-  if (type == 'joinVoiceChannel') lastJoinVoiceChannel = Math.floor(Date.now() / 1000 / 60);
-  if (type == 'joinStageChannel') lastJoinStageChannel = Math.floor(Date.now() / 1000 / 60);
+  if (type == 'joinVoiceChannel') lastJoinVoiceChannel[userId] = Math.floor(Date.now() / 1000 / 60);
+  if (type == 'joinStageChannel') lastJoinStageChannel[userId] = Math.floor(Date.now() / 1000 / 60);
 
-  if (type == 'leftVoiceChannel' && lastJoinVoiceChannel != null)
+  if (type == 'leftVoiceChannel' && lastJoinVoiceChannel[userId] != null)
     setData(guildId, ['memberData', 'time', 'inVoiceChannel', userId],
-      Math.floor(Date.now() / 1000 / 60) - lastJoinVoiceChannel, '+');
-  if (type == 'leftStageChannel' && lastJoinStageChannel != null)
+      Math.floor(Date.now() / 1000 / 60) - lastJoinVoiceChannel[userId], '+');
+  if (type == 'leftStageChannel' && lastJoinStageChannel[userId] != null)
     setData(guildId, ['memberData', 'time', 'inStageChannel', userId],
-      Math.floor(Date.now() / 1000 / 60) - lastJoinStageChannel, '+');
+      Math.floor(Date.now() / 1000 / 60) - lastJoinStageChannel[userId], '+');
 }
