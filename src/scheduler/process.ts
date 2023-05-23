a4093c1faddf1a66b4f8bb40b471ebc3bf5782a7
yuyu <hcsd7b34@gmail.com>
2023-05-23 19:37:34 +0900

    Change: timerのメンションをユーザーのみに変更

--- src/scheduler/process.ts
+++ src/scheduler/process.ts
@@ -5,7 +5,12 @@ export default function (type: string, data: any) {
     case 'end-timer': {
       const channel = client.channels.cache.get(data.channel);
       if (channel == null || !('send' in channel)) return;
-      channel.send(data.message == '' ? ':alarm_clock:タイマーが鳴りました' : data.message);
+      channel.send({
+        content: data.message == '' ? ':alarm_clock:タイマーが鳴りました' : data.message,
+        allowedMentions: {
+          parse: ['users']
+        }
+      });
     }
     break;
   }
