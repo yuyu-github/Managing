4c0f6fef1ad4e2f7f54299074787ecbe930ccd46
yuyu <hcsd7b34@gmail.com>
2023-05-23 19:44:37 +0900

    Dev: googlechartsnodeの型情報を追加

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,11 @@
-declare module 'google-charts-node';
+declare module 'google-charts-node' {
+  type RenderOptions = {
+    width?: string,
+    height?: string,
+    packages?: string[],
+    mapsApiKey?: string,
+    puppeteerOptions?: any
+  }
+
+  function render(drawChartFunction: string | Function, options: RenderOptions): Promise<Buffer>
+}
