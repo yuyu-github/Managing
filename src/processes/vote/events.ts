ba974db750ce306ff5500673969f73ce934eba79
yuyu <hcsd7b34@gmail.com>
2023-06-07 08:07:44 +0900

    Fix: 重複の判定処理が遅い

--- src/processes/vote/events.ts
+++ src/processes/vote/events.ts
@@ -1,4 +1,4 @@
-import { Client, MessageReaction, PartialMessageReaction, PartialUser, ReactionManager, User } from 'discord.js';
+import { Client, Collection, MessageReaction, PartialMessageReaction, PartialUser, ReactionManager, User } from 'discord.js';
 
 import { setData, getData, deleteData } from 'discordbot-data';
 
@@ -28,21 +28,33 @@ export async function onReactionAdd(reaction: MessageReaction | PartialMessageRe
     let reactionCount = 0;
     let reactionMemberCount = 0;
     let reactionMembers: string[] = []
+    let promises: Promise<any>[] = []
     for (let item of reaction.message.reactions.cache) {
       reactionCount += item[1].count - (item[1].users.cache.has(client.user?.id ?? '') ? 1 : 0);
 
-      for (let id of (await item[1].users.fetch()).keys()) {
-        if (!reactionMembers.includes(id) && id != client.user?.id) {
-          reactionMemberCount++;
-          reactionMembers.push(id);
+      function duplicateCheck(users: Collection<string, User>) {
+        if (!(vote.multiple ?? false) && item[0] != reaction.emoji.name && users.has(user.id)) {
+          item[1].users.remove(user);
+          reactionCount--;
+          return true;
         }
+        return false;
       }
+      let hadDuplicate = duplicateCheck(item[1].users.cache)
 
-      if (!(vote.multiple ?? false) && item[0] != reaction.emoji.name && item[1].users.cache.has(user.id)) {
-        item[1].users.remove(user);
-        reactionCount--;
-      }
+      promises.push(reaction.users.fetch().then(users => {
+        if (!hadDuplicate) duplicateCheck(users);
+        if (vote.count > 0) {
+          for (let id of users.keys()) {
+            if (!reactionMembers.includes(id) && id != client.user?.id) {
+              reactionMemberCount++;
+              reactionMembers.push(id);
+            }
+          }
+        }
+      }))
     }
+    await Promise.all(promises);
 
     if (onReactionAddFn[vote.type as VoteType]?.(vote, reaction, user, reactionCount, reactionMemberCount)) {
       deleteData('guild', reaction.message.guildId!, ['vote', 'list', reaction.message.channelId, reaction.message.id])
